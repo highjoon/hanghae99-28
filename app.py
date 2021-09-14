aa4d92df8a4f00079df2d525cdc79c539dd01b12
YoonSangJoon <63948484+highjoon@users.noreply.github.com>
2021-09-14 16:48:49 +0900

    review markup

--- app.py
+++ app.py
@@ -19,5 +19,10 @@ def main():
     return render_template("index.html")
 
 
+@app.route('/review')
+def review():
+    return render_template("review.html")
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
