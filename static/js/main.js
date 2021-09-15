89d1bb47871f6914346308ea8b208e46b07dbfb3
YoonSangJoon <63948484+highjoon@users.noreply.github.com>
2021-09-15 19:41:23 +0900

    검색 에러 해결

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,25 @@
 function find_camp() {
-    let camp = $(".home__form--input").val();
-    let boot_name = "";
-    let boot_img = "";
-
+    let camp = document.querySelector(".home__form--input").value;
     if (!camp) {
-        alert("잘못된 입력입니다!");
-        location.reload();
+        showError();
         return;
     }
 
-    try {
-        for (let i = 0; i < campList.length; i++) {
-            if (campList[i]["name"] === camp) {
-                boot_name = campList[i]["name"];
-                boot_img = campList[i]["boot_img"];
-                boot_id = campList[i]["id"];
-                break;
-            }
+    let boot_name = "";
+    let boot_img = "";
+    let boot_id = "";
+    for (let i = 0; i < campList.length; i++) {
+        if (campList[i]["name"] === camp) {
+            boot_name = campList[i]["name"];
+            boot_img = campList[i]["boot_img"];
+            boot_id = campList[i]["id"];
+            break;
+        } else {
+            boot_id = false;
         }
+    }
 
+    if (boot_id) {
         let targetList = document.querySelectorAll(".card");
         targetList.forEach((target) => {
             let target_id = target.attributes[2].nodeValue;
@@ -30,10 +31,9 @@ function find_camp() {
                 target.classList.remove("show");
             }
         });
-    } catch (error) {
-        alert("잘못된 입력입니다!");
-        location.reload();
-        return;
+    } else {
+        showError();
+        camp.innerText = '';
     }
 }
 
@@ -69,4 +69,9 @@ function createCampList(camps) {
     camps.forEach((camp) => {
         campList.push(camp);
     });
+}
+
+function showError() {
+    alert('잘못된 입력입니다!');
+    document.querySelector(".home__form--input").value = '';
 }
